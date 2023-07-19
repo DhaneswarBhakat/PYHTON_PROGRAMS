a2aee269d70c3afc67a8d2fe1e97c5a1d0eead86
Rahul <rsrahulsalunkhe4@gmail.com>
2023-07-19 18:19:18 +0530

    practical 1 is update

--- Develop programs to understand the control structures of python/P1.py
+++ Develop programs to understand the control structures of python/P1.py
@@ -1,51 +1,36 @@
-# Control Structure Example
+import random
 
-# if-else statement
-age = 25
-if age >= 18:
-    print("You are an adult.")
+print("What do you choose? Type 0 for Rock, 1 for Paper or 2 for Scissors.")
+num = int(input())
+
+if num == 0:
+  print("rock")
+elif num == 1:
+  print("paper")
 else:
-    print("You are a minor.")
+  print("scissors")
 
-# if-elif-else statement
-grade = 75
-if grade >= 90:
-    print("You got an A.")
-elif grade >= 80:
-    print("You got a B.")
-elif grade >= 70:
-    print("You got a C.")
+com_num = random.randint(0,2)
+
+print("Computer chose:")
+if com_num == 0:
+  print("rock")
+elif com_num == 1:
+  print("paper")
 else:
-    print("You need to improve.")
+  print("scissors")
 
-# for loop
-numbers = [1, 2, 3, 4, 5]
-sum = 0
-for num in numbers:
-    sum += num
-print("Sum of numbers:", sum)
-
-# while loop
-count = 0
-while count < 5:
-    print("Count:", count)
-    count += 1
-
-# break and continue statements
-for i in range(10):
-    if i == 3:
-        continue  # Skip the rest of the code in the loop and move to the next iteration
-    if i == 7:
-        break  # Exit the loop completely
-    print(i)
-
-# Pass Statement Example
-
-numbers = [1, 2, 3, 4, 5]
-
-for num in numbers:
-    if num % 2 == 0:
-        pass  # Placeholder, no action needed for even numbers
-    else:
-        print(num)  # Print odd numbers
-
+if num == 0 and com_num == 1:
+  print("You Loss")
+elif num == 0 and com_num == 2:
+  print("You Win")
+elif num == 1 and com_num == 0:
+  print("You Win")
+elif num == 1 and com_num == 2:
+  print("You Loss")
+elif num == 2 and com_num == 0:
+  print("You Loss")
+elif num == 2 and com_num == 1:
+  print("You Win")
+else:
+  print("It's a draw")
